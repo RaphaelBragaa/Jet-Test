016b93d24443f72be8db985b4d2094bdbc0275b6
Raphael <rbragamouraa@gmail.com>
2025-07-19 22:04:58 -0300

    feat: app.ts para iniciar a api com o banco de dados

--- Back-End/src/app.ts
+++ Back-End/src/app.ts
@@ -1,1 +1,22 @@
+import express from 'express'
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { connectToDatabase } from './database/db';
 
+dotenv.config();
+
+const server = express()
+server.use(cors())
+      .use(express.json())
+
+
+connectToDatabase()
+  .then(() => {
+    server.listen(process.env.PORT, () => {
+      console.log(`Server On! Porta: ${process.env.PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error('Falha na inicialização:', err);
+    process.exit(1);
+  });
